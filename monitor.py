4db58d6c3ac8b6b8d2cc9ef5a25315ff997dc733
Sliently Continue <SlientlyContinue@mailas.com>
2018-07-26 17:26:58 +0100

    Add developer friendly websites

--- monitor.py
+++ monitor.py
@@ -3,7 +3,7 @@ from gpiozero.tools import negated, smoothed
 import requests
 from time import sleep
 
-sb = StatusBoard('raspberrypi', 'codeclub', 'coderdojo')
+sb = StatusBoard('google', 'stackoverflow', 'github', 'azure', 'granta')
 
 def website_up(url):
     while True:
@@ -15,19 +15,21 @@ def website_up(url):
             yield False
 
 statuses = {
-    sb.raspberrypi: website_up('https://www.raspberrypi.org/'),
-    sb.codeclub: website_up('https://www.codeclub.org.uk/'),
-    sb.coderdojo: website_up('https://www.coderdojo.com/'),
+    sb.google: website_up('https://google.com/'),
+    sb.stackoverflow: website_up('https://stackoverflow.com/'),
+    sb.github: website_up('https://github.com/ErrorActionPreference/'),
+    sb.azure: website_up('https://azure.microsoft.com/'),
+    sb.granta: website_up('https://grantadesign.com/'),
 }
 
 for strip, website in statuses.items():
     strip.lights.green.source = smoothed(website, 2, any)  # allow 1 false negative out of 2
     strip.lights.green.source_delay = 60
+
     strip.lights.red.source = negated(strip.lights.green.values)
     strip.lights.red.source_delay = 60
 
-while True:
-    if not website_up('https://www.google.com/'):
-        for strip in sb:
-            strip.lights.blink()
-    sleep(60)
+
+
+input()
+
