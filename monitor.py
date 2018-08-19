52ae1177090d2b16a9acbc3d8d1a2f8e070e49a8
Sliently Continue <SlientlyContinue@mailas.com>
2018-08-19 10:25:08 +0100

    Test google

--- monitor.py
+++ monitor.py
@@ -4,32 +4,6 @@ import requests
 from time import sleep
 
 
-def pulse_leds(leds, fade_in=0, fade_out=0.7, pause=.25):
-    for led in leds:
-        led.pulse(fade_in, fade_out, 1, True)
-        if pause > 0:
-            sleep(pause)
-
-green_leds = LEDBoard(17, 22, 9, 5, 13, pwm=True)
-red_leds = LEDBoard(4, 27, 10, 11, 6, pwm=True)
-
-red_leds.off()
-green_leds.off()
-
-pulse_leds(red_leds, fade_in=2, pause=0)
-
-sleep(1)
-
-for repeat in range(5):
-    pulse_leds(red_leds)
-    pulse_leds(reversed(red_leds))
-
-red_leds.close()
-green_leds.close()
-
-
-status_board = StatusBoard('google', 'stackoverflow', 'github', 'azure', 'granta', )
-
 def is_website_up(url):
     try:
         r = requests.get(url)
@@ -37,6 +11,7 @@ def is_website_up(url):
         return r.ok
     except:
         return false
+
 
 def website_up(url):
     while True:
@@ -47,10 +22,59 @@ def website_up(url):
         except:
             yield False
 
+
+def pulse_leds(leds, fade_in=1.0, fade_out=1.0, pause=0.0):
+    for led in leds:
+        led.pulse(fade_in, fade_out, 1, True)
+        if pause > 0:
+            sleep(pause)
+
+
+green_leds = LEDBoard(17, 22, 9, 5, 13, pwm=True)
+red_leds = LEDBoard(4, 27, 10, 11, 6, pwm=True)
+
+red_leds.off()
+green_leds.off()
+
+# Pulse red leds on
+pulse_leds(red_leds)
+
+sleep(2)
+
+# Swoosh up down
+fade_in = 0
+fade_out = 0.7
+pause = 0.25
+
+for repeat in range(5):
+    pulse_leds(red_leds, fade_in, fade_out, pause)
+    pulse_leds(reversed(red_leds), fade_in, fade_out, pause)
+
+# Detect web sites
+green_led = green_leds[0]
+red_led = red_leds[0]
+
+green_led.blink(0.5, 0.5)
+sleep(3)
+if is_website_up('https://google.com'):
+    green_led.on()
+    red_led.off()
+else:
+    green_led.off()
+    red_led.on()
+
+while True:
+    # Do nothing
+    pass
+
+red_leds.close()
+green_leds.close()
+
+status_board = StatusBoard('google', 'stackoverflow', 'github', 'azure', 'granta', )
 statuses = {
-    status_board.google:        'https://google.com/',
+    status_board.google: 'https://google.com/',
     status_board.stackoverflow: 'https://stackoverflow.com/',
-    status_board.github:        'https://github.com/ErrorActionPreference/',
-    status_board.azure:         'https://azure.microsoft.com/',
-    status_board.granta:        'https://grantadesign.com/',
+    status_board.github: 'https://github.com/ErrorActionPreference/',
+    status_board.azure: 'https://azure.microsoft.com/',
+    status_board.granta: 'https://grantadesign.com/',
 }
